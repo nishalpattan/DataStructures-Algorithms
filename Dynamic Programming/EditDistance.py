085fe6c92a8520819df2d8a747677018a68ba7cc
nishal <nishal.pattan@gmail.com>
2020-09-21 10:40:22 -0400

    back tracking

---
+++ Dynamic Programming/EditDistance.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+        """
+        https://leetcode.com/problems/edit-distance/submissions/
+
+        Dynamic Programming Appraoch :
+        m --> len(word1)
+        n --> len(word2)
+        TC : O( m * n )
+        SC : O (m * n)
+        """
+        m = len(word1)
+        n = len(word2)
+        dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+
+        for i in range(len(word1) + 1):
+            for j in range(len(word2) + 1):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
+                elif word1[i - 1] != word2[j - 1]:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                else:
+                    dp[i][j] = dp[i - 1][j - 1]
+        return dp[m][n]
