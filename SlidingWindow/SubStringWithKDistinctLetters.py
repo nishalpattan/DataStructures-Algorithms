91fb3a777c64933fab3f6d51a71a74294ab11923
nishal <nishal.pattan@gmail.com>
2020-09-26 14:28:47 -0400

    Substrings with k distinct chars

---
+++ SlidingWindow/SubStringWithKDistinctLetters.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/discuss/interview-question/370112
+
+Problem similar to Longest SubString without Repeating Characters
+
+TC : O(n)
+SC : O(n)
+"""
+
+
+def substrings_with_k_unique_chars(s, k):
+    hash_map = dict()
+    start = 0
+    res = set()
+
+    for idx, char in enumerate(s):
+        if char in hash_map:
+            start = max(hash_map[char] + 1, start)
+        if idx+1 - start == k:
+            res.add(s[start : idx+1])
+            start += 1
+        hash_map[char] = idx
+    return list(res)
+
+if __name__ == "__main__":
+    print(substrings_with_k_unique_chars("abcabc", 3))
+    print(substrings_with_k_unique_chars("aa", 1))
+
