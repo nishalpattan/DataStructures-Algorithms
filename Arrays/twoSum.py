d4c12196e2a49f6eb0d18186a6e0c35d34d74059
nishal <nishal.pattan@gmail.com>
2021-10-14 19:02:17 -0400

    two sum sorted array

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -1,12 +1,32 @@
 def twoNumberSum(array, targetSum):
-    # Write your code here.
-    # TC : O(n), SC : O(1)
+	"""
+    Time Complexity : O(n)
+    Space Complexity : O(n)
+    :param array:
+    :param targetSum:
+    :return:[index1, index2]
+    """
 	hash_map = dict()
 	for num in array:
 		if num in hash_map:
 			return [num, targetSum - num]
 		hash_map[targetSum - num] = num
 	return []
+
+def twoNumberSumSortedArray(array, targetSum):
+	"""
+	Time Complexity : O(n)
+	Space Complexity : O(1)
+	:param array:
+	:param targetSum:
+	:return: [index1, index2]
+
+	Use two pointer method to traverse in a sorted array(non-decreasing),
+	if the sum of numbers at start and end index equals target sum, return those indexes
+	else if the sum is less than target sum, increment start index , following the increasing order of sorted array
+	else if the sum is greater than target sum, decrement end index.
+	"""
+
 
 if __name__ == "__main__":
     print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
---
+++ Recursion/PowerOfNumber.py
@@ -0,0 +1,7 @@
+def power(a,b):
+    if b==0:
+        return 1
+    return a * power(a, b-1)
+
+if __name__ == "__main__":
+    print(power(3,2))
